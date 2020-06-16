908d46894b390f84f75bdb1fcfb87d95df50011d
longl <longl@rocketsoftware.com>
2020-06-16 14:23:13 +0800

    Add, provide, inject service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
-import { HEROES } from '../mock-heroes';
+//import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -8,19 +9,25 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  heroes: Hero[];
+  constructor(private heroService: HeroService) { }
   hero: Hero = {
-    id: 0,
-    name: 'Windestorm'
-  };
+  id: 0,
+  name: 'Windestorm'
+};
 
-  heroes = HEROES;
+  //heroes = HEROES;
 
   selectedHero: Hero;
   onSelect(hero: Hero): void{
     this.selectedHero = hero;
   }
+
+  getHeroes(): void{
+    this.heroes = this.heroService.getHeroes();
+  }
   ngOnInit(): void {
+    this.getHeroes();
   }
 
 }
